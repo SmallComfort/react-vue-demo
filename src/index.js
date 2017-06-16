488d864c70d1a468882985e83e7b9427bbea6c58
“张创” <799637494@qq.com>
2017-06-16 23:10:56 +0800

    remove server render

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import App from './App';
 
 import ReactDOMServer from 'react-dom/server';
 
-const S = ReactDOMServer.renderToString(<App />)
-const SS = ReactDOMServer.renderToStaticMarkup(<App />)
-console.log(S)
-console.log(SS)
-
 ReactDOM.render(<App />, document.getElementById('app'));

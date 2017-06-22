e80645c9aba4ab78db13cbe24c77a4e77942a7ba
“张创” <799637494@qq.com>
2017-06-22 22:24:11 +0800

    update package.json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import ReactDOMServer from 'react-dom/server';
-
 ReactDOM.render(<App />, document.getElementById('app'));

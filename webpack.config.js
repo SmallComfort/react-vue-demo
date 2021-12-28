26dab5ae54ce77887874f81ebc711abf1669bdb5
Konstantin Panfilov <error500@error500.ru>
2021-12-28 19:43:29 +0300

    Refactorin package.json depends & fix .babelrc & rebuild webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,114 @@
 const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    build: './src/index.js',
-    vendor: [
-      'react',
-      'react-dom',
-      'react-vue',
-      'react-vue-helper',
-      'vuex',
-      'prop-types'
-    ]
-  },
+    mode: "development",
+    entry: {
+        build: './src/index.js',
+        vendor: [
+            'react',
+            'react-dom',
+            'react-vue',
+            'react-vue-helper',
+            'vuex',
+            'prop-types'
+        ]
+    },
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/dist/'
-  },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '',
+        clean: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader']
-      },
-      {
-        test: /\.vue$/,
-        loader: 'react-vue-loader',
-        options: {
-          vue: './vue.config.js'
-        }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'react-vue-loader',
+                options: {
+                    vue: './vue.config.js'
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [{
+                    loader: "style-loader",
+                    options: {
+                        injectType: 'linkTag',
+                        insert: 'head',
+                        attributes: { id: "id" }
+                    }
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true,
+                        /*modules: {
+                            namedExport: true,
+                            localIdentName: '[local]_[hash:base64:8]',
+                            localIdentContext: path.resolve(__dirname, "src")
+                        },*/
+
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        minimize: false
+                    }
+                }]
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',    // where the fonts will go
+                    }
+                }]
+            },
+            {
+                test: /\.(gif|png|jpe?g)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',    // where the fonts will go
+                    }
+                }]
+            }]
+    },
+
+    resolve: {
+        extensions: ['.vue', '.js']
+    },
+
+      plugins: [
+          new HtmlWebpackPlugin({
+              publicPath: './',
+              inject:false,
+              template: 'public/index.html',
+              meta: {
+                charset: { charset: 'utf-8' },
+                viewport: 'width=device-width, initial-scale=1'
+              },
+              minify: false
+          })
+      ],
+
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
       }
-    ]
-  },
-
-  resolve: {
-    extensions: ['.vue', '.js']
-  },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ]
 };
